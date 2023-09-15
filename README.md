d6118e96527228a8eff08077ed0e9f5388a93131
Jimmie Munyi <jimmiemunyi@gmail.com>
2023-09-15 09:37:19 +0300

    new database orders_db

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Orders API Documentation
 Deployed on Heroku - not done
+
+
+# Postgres
+
+```bash
+sudo su - postgres
+psql
+```
+
+```
+CREATE testuser WITH PASSWORD testpass;
+CREATE DATABASE orders_db WITH OWNER testuser ENCODING='UTF8';
+CREATE DATABASE orders_api_db_test WITH OWNER testuser ENCODING='UTF8';
+```
--- migrations/env.py
+++ migrations/env.py
@@ -93,7 +93,6 @@ def run_migrations_online():
         context.configure(
             connection=connection,
             target_metadata=get_metadata(),
-            # compare_type=True,
             process_revision_directives=process_revision_directives,
             **current_app.extensions["migrate"].configure_args
         )
